aa616655bf54e85a4ad6fef2590a7d47b5d610fc
Joseph Ani <anijoe688@gmail.com>
2025-05-04 12:32:37 +0100

    fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,6 @@
 import { motion } from "framer-motion"
 import Image from "next/image"
 import BackArrow from "../Components/BackArrow"
-import { useRouter } from "next/navigation"
 import { useState, useEffect } from "react"
 import ClientMap from '../Components/ClientMap'
 

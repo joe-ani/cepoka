3dd3414257b0005ad4dfe698d646f7f7015626ff
Joseph Ani <anijoe688@gmail.com>
2025-05-17 00:17:11 +0100

    refactor: Remove Layout component and create Template component for better structure

--- src/app/admin/stock-manager/[id]/layout.tsx
+++ src/app/admin/stock-manager/[id]/layout.tsx
@@ -1,13 +1,5 @@
 import { Metadata } from 'next';
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
-
 export async function generateMetadata({
   params
 }: {
